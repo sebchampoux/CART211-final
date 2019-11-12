34f46b291220372e1ec1343c9a87999907995585
gamergirl69 <56971054+gamergirl69@users.noreply.github.com>
2019-11-11 21:32:53 -0500

    Vision update
    Added top menu and scroll back up button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', e => {
-	
+    	
 });
+
+//js for the scroll back up button
+backUpButton = document.getElementsByClassName("backUp");
+
+window.onscroll = function() {scrollDown()};
+
+function scrollDown() {
+  if (document.body.scrollToTop > 20 || document.documentElement.scrollToTop > 20) {
+    backUpButton.style.display = "block";
+  } else {
+    backUpButton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollToTop = 0; 
+  document.documentElement.scrollToTop = 0; 
+} 
