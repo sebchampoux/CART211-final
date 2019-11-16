25184e6904eee07026d4e03840d7223b4db95020
Sébastien Champoux <contact@sebastienchampoux.com>
2019-11-16 16:14:50 -0500

    Fixed indentation in js file

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
 document.addEventListener('DOMContentLoaded', e => {
+	//js for the scroll back up button
+	var backUpButton = document.getElementById("backUpBtn");
 
+	function scrollDown() {
+		if (window.scrollY > 20) {
+			backUpButton.style.display = "block";
+		}
+		else {
+			backUpButton.style.display = "none";
+		}
+	}
 
-//js for the scroll back up button
-var backUpButton = document.getElementById("backUpBtn");
+	function goToTop() {
+		window.scrollTo({ top: 0 });
+	}
 
-window.onscroll = function() {scrollDown()};
-
-function scrollDown() {
-  if (window.scrollY > 20 ) {
-    backUpButton.style.display = "block";
-  } 
-  else {
-    backUpButton.style.display = "none";
-  }
-}
-
-backUpButton.addEventListener("click",goToTop);
-
-function goToTop() {
-  window.scrollTo({top:0}); 
-}
+	window.onscroll = function () { scrollDown() };
+	backUpButton.addEventListener("click", goToTop);
 
 });
-
- 
