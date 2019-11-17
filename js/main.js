a254f935911eed1c0cf6a43571bd9afe42f4bef1
Sébastien Champoux <contact@sebastienchampoux.com>
2019-11-17 00:57:34 -0500

    Started animating the front page

--- js/main.js
+++ js/main.js
@@ -8,7 +8,30 @@ document.addEventListener('DOMContentLoaded', e => {
 });
 
 function homePage() {
-	console.log('hello, world !');
+	let heroTl = new TimelineMax({ paused: true });
+	var statsTl = new TimelineMax({ paused: true });
+
+	var halfHalftl = new TimelineMax({ delay: 1 });
+	var halfHalfBG = document.querySelector('.half-half__content-bg');
+	halfHalftl.set(halfHalfBG, { transformOrigin: '0 0' });
+
+	halfHalftl.addLabel('in-bgs');
+	halfHalftl.from(halfHalfBG, 0.6, { scaleX: 0 }, 'in-bgs');
+	halfHalftl.add(tlAnimatedImage(document.querySelector('.half-half__image')), 'in-bgs+=0.2');
+
+	halfHalftl.staggerFrom('.half-half__title, .half-half__text, .half-half .button', 1, { opacity: 0, ease: Power2.easeInOut }, 0.15);
+}
+
+function tlAnimatedImage(animatedImageObject) {
+	var inner = animatedImageObject.querySelector('.animated-image__inner');
+	var img = animatedImageObject.querySelector('.animated-image__img');
+	var duration = 0.8;
+
+	let tl = new TimelineMax();
+	tl.addLabel('img-in');
+	tl.from(inner, duration, { x: '-100%' }, 'img-in');
+	tl.from(img, duration, { x: '100%' }, 'img-in');
+	return tl;
 }
 
 function visionPage() {
